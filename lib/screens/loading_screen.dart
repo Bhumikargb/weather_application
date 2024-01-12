39dc6b9d03cc53c5dfda8e50269d2d96ae012fe0
Bhumika Khanna <khannabhumika108@gmail.com>
2024-01-12 21:49:55 +0530

    Initial commit

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,46 @@
+
+
+import 'package:flutter/material.dart';
+import '../services/weather.dart';
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:clima_flutter_weather_app/services/weather.dart';
+
+// const apiKey='9ac46842f3f7dc7df0f8cef2d03415f3';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LoadingScreenState();
+  }
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    var weatherData = await WeatherModel().getLocationWeather();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(
+        locationWeather: weatherData,
+      );
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
+  }
+}
